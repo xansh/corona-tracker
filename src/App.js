ad342fe75585e7232cd1183353a19bae1a1a6b67
Ansh Kumar <betul.kumar@gmail.com>
2020-05-17 18:39:57 +0530

    Fetched virus data from api thevirustracker.com on component mount

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const virusData = {
-  India: {
-    infected: 82000,
-    deaths: 100,
-    recovered: 40000
-  },
-  China: {
-    infected: 270000,
-    deaths: 20000,
-    recovered: 100000
-  },
-  Russia: {
-    infected: 90000,
-    deaths: 6000,
-    recovered: 50000
-  },
-  Japan: {
-    infected: 75000,
-    deaths: 6700,
-    recovered: 38000
-  },
-  Pakistan: {
-    infected: 1,
-    deaths: 0,
-    recovered: 0
-  }
-}
-
+const url = 'https://api.thevirustracker.com/free-api?countryTotals=ALL';
 
 class App extends React.Component {
-  state = {country: 'India'};
-  stats = (event) => {
-     this.setState({country: event.target.value});
+  state = {selectedCountry: '', allData: {}, loading: true, success: false};
+  async componentDidMount()
+  {
+    await fetch(url)
+    .then(async (response) => {
+      const responseData = await response.json();
+      let virusData = responseData.countryitems[0];
+      let modifiedVirusData = {};
+      Object.keys(virusData).forEach((key) => {
+        let countryData = virusData[key];
+        const code = countryData.code;
+        delete countryData.code;
+        delete countryData.source;
+        delete countryData.ourid;
+        modifiedVirusData[code] = countryData;
+      });
+      this.setState({success: true, allData: modifiedVirusData, selectedCountry: Object.keys(modifiedVirusData)[0]});
+    })
+    .catch((error) => {
+      console.log(error);
+      this.setState({success: false});
+    });
+    this.setState({loading: false});
   }
+  stats = (event) => {
+     this.setState({selectedCountry: event.target.value});
+  }
+  prettify = (name) => (name.split('_').join(' '));
   render() {
-    const {country} = this.state;
+    const {selectedCountry, allData, loading, success} = this.state;
     return (
       <div className="App">
-        <label>Countries</label>
-        <br/>
-        <select onChange = {this.stats} value={country}>
-          {Object.keys(virusData).map((element) => (<option key={element} value={element}>{element}</option>))}
-        </select>
-        <div>Infected = {virusData[country].infected}</div>
-        <br/>
-        <div>Deaths = {virusData[country].deaths}</div>
-        <br/>
-        <div>Recovered = {virusData[country].recovered}</div>
+        {loading?
+          (<h1>Loading...</h1>) :
+          (<div>
+            {success?
+              (<div>
+                <h2>Countries</h2>
+                <br/>
+                <select onChange = {this.stats} value={selectedCountry}>
+                  {Object.keys(allData).map((element) => (<option key={element} value={element} label={allData[element].title}/>))}
+                </select>
+                {Object.keys(allData[selectedCountry]).filter(key => key !== 'title').map((key) => {
+                  return (<div key={key}>
+                    <br/>
+                    <div>{this.prettify(key)} = {allData[selectedCountry][key]}</div>
+                  </div>);
+                })}
+              </div>) : alert("Error loading data")
+            }
+        </div>)
+        }
       </div>
     );
   }
