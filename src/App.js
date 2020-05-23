807215b851dc959a6dcbd750c2e15ec0d0d57704
Ansh Kumar <betul.kumar@gmail.com>
2020-05-23 21:48:31 +0530

    Added country wise timeline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactFlagsSelect from 'react-flags-select';
 import 'react-flags-select/css/react-flags-select.css';
+import LineChart, {parseFlatArray} from 'react-linechart';
+import '../node_modules/react-linechart/dist/styles.css';
 import './App.css';
 import StatBox from './StatBox';
 
@@ -8,13 +10,20 @@ const url = 'https://api.thevirustracker.com/free-api?countryTotals=ALL';
 const defaultCountry = 'IN';
 
 class App extends React.Component {
-  state = {selectedCountry: '', allData: {}, loading: true, success: false};
+  state = {
+    selectedCountry: '',
+    allData: {},
+    loading: true,
+    success: false,
+    chartData: [],
+    chartLoading: true,
+    chartSuccess: false
+  };
   async componentDidMount()
   {
     await fetch(url)
     .then(async (response) => {
-      const responseData = await response.json();
-      let virusData = responseData.countryitems[0];
+      const virusData = (await response.json()).countryitems[0];
       let modifiedVirusData = {};
       Object.keys(virusData).forEach((key) => {
         let countryData = virusData[key];
@@ -30,13 +39,38 @@ class App extends React.Component {
       console.log(error);
       this.setState({success: false});
     });
+    this.onSelectCountry(defaultCountry);
     this.setState({loading: false});
   }
   onSelectCountry = (value) => {
-     this.setState({selectedCountry: value});
+     this.setState({selectedCountry: value, chartLoading: true});
+     fetch(`https://corona-api.com/countries/${value}`)
+     .then(async (response) => {
+       const responseData = (await response.json()).data.timeline;
+       const chartData = parseFlatArray(responseData,
+                                        'date',
+                                        ['confirmed', 'active', 'deaths', 'recovered'],
+                                        ['black', 'blue', 'red', 'green'],
+                                        undefined,
+                                        ['Total', 'Active', 'Deaths', 'Recovered']);
+       console.log(chartData);
+       this.setState({chartSuccess: true, chartData});
+     })
+     .catch((error) => {
+       console.log(error);
+       this.setState({chartSuccess: false});
+     })
+     .finally(() => {
+       this.setState({chartLoading: false});
+     });
   }
   render() {
-    const {selectedCountry, allData, loading, success} = this.state;
+    const {
+      selectedCountry,
+      allData,
+      loading, success,
+      chartData,
+      chartLoading, chartSuccess} = this.state;
     return (
       <div className="App">
         {loading?
@@ -44,31 +78,53 @@ class App extends React.Component {
           (<div>
             {success?
               (<div>
-                <h1>Countrywise Corona Tracker</h1>
-                <br/>
-                <h2>Select A Country</h2>
-                <br/>
-                <ReactFlagsSelect
-                  searchable
-                  onSelect={this.onSelectCountry}
-                  countries={Object.keys(allData)}
-                  defaultCountry={selectedCountry}
-                />
-                <br/>
+                <div>
+                  <h1>Countrywise Corona Tracker</h1>
+                  <br/>
+                  <h2>Select A Country</h2>
+                  <br/>
+                  <ReactFlagsSelect
+                    searchable
+                    onSelect={this.onSelectCountry}
+                    countries={Object.keys(allData)}
+                    defaultCountry={selectedCountry}
+                  />
+                  <br/>
                   <StatBox
                     title={'Total Cases'}
                     value={allData[selectedCountry].total_cases}
-                    newValue={allData[selectedCountry].total_new_cases_today}/>
+                    newValue={allData[selectedCountry].total_new_cases_today}
+                  />
                   <StatBox
                     title={'Total Active Cases'}
-                    value={allData[selectedCountry].total_active_cases}/>
+                    value={allData[selectedCountry].total_active_cases}
+                  />
                   <StatBox
                     title={'Total Recovered'}
-                    value={allData[selectedCountry].total_recovered}/>
+                    value={allData[selectedCountry].total_recovered}
+                  />
                   <StatBox
                     title={'Total Deaths'}
                     value={allData[selectedCountry].total_deaths}
-                    newValue={allData[selectedCountry].total_new_deaths_today}/>
+                    newValue={allData[selectedCountry].total_new_deaths_today}
+                  />
+                </div>
+                <div>
+                  {chartLoading ?
+                    <h1>Loading Chart...</h1> :
+                    chartSuccess ?
+                      <LineChart
+                        data={chartData}
+                        xLabel='Date'
+                        yLabel=''
+                        isDate
+                        showLegends
+                        pointRadius={2}
+                      /> :
+                      <h2 style={{color: 'red'}}>Unable to load chart</h2>
+                  }
+                </div>
+                <br/>
               </div>) : alert("Error loading data")
             }
         </div>)
--- src/StatBox.js
+++ src/StatBox.js
@@ -16,13 +16,16 @@ class StatBox extends React.Component {
       <br/>
       <div className="StatBox-title">{this.prettify(this.props.title)}</div>
       <br/>
-      <div>
-        <CountUp className="StatBox-data" end={this.props.value} duration={1.5} separator=','/>
-        {this.props.newValue ?
-          <CountUp className="StatBox-new-data" prefix=' +' end={this.props.newValue} duration={1.5} separator=','/>
-          :null
-        }
-      </div>
+      {this.props.value > 0?
+        <div>
+          <CountUp className="StatBox-data" end={this.props.value} duration={1.5} separator=','/>
+          {this.props.newValue ?
+            <CountUp className="StatBox-new-data" prefix=' +' end={this.props.newValue} duration={1.5} separator=','/>
+            :null
+          }
+        </div> :
+        <div> N/A </div>
+      }
     </div>);
   }
 }
